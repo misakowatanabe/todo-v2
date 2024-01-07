18857679f7bbfbd0d74457a711ea1e0020a94277
misakowatanabe <misakolani@gmail.com>
2024-01-07 13:37:54 +0100

    updated .eslintrc.json, .lintstagedrc.js. Now get error by attemting commit with non formatted files

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,8 +3,6 @@ const path = require('path')
 const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`
 
-const formatCommand = 'prettier --write'
-
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand, formatCommand],
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
 }
