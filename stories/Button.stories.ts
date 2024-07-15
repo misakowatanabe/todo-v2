086d009ed972791fe5c09355e5b9b91f5875e8e9
Misako Watanabe <misakolani@gmail.com>
2024-07-15 22:18:12 +0200

    storybook upgrade conflict fix

--- stories/Button.stories.ts
+++ stories/Button.stories.ts
@@ -1,10 +1,9 @@
 import type { Meta, StoryObj } from '@storybook/react'
-
 import { Button } from './Button'
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
-const meta = {
-  title: 'Example/Button',
+const meta: Meta<typeof Button> = {
+  title: 'Design System/Button',
   component: Button,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
@@ -16,10 +15,10 @@ const meta = {
   argTypes: {
     backgroundColor: { control: 'color' },
   },
-} satisfies Meta<typeof Button>
+}
 
 export default meta
-type Story = StoryObj<typeof meta>
+type Story = StoryObj<typeof Button>
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
--- stories/Page.stories.ts
+++ stories/Page.stories.ts
@@ -3,21 +3,19 @@ import { expect, userEvent, within } from '@storybook/test'
 
 import { Page } from './Page'
 
-const meta = {
-  title: 'Example/Page',
+const meta: Meta<typeof Page> = {
+  title: 'Design System/Page',
   component: Page,
   parameters: {
-    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
     layout: 'fullscreen',
   },
-} satisfies Meta<typeof Page>
+}
 
 export default meta
-type Story = StoryObj<typeof meta>
+type Story = StoryObj<typeof Page>
 
 export const LoggedOut: Story = {}
 
-// More on interaction testing: https://storybook.js.org/docs/writing-tests/interaction-testing
 export const LoggedIn: Story = {
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement)
