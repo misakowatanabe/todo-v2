cce363ebefb29379f3ba75fbff51fbbaab0279cc
Misako Watanabe <misakolani@gmail.com>
2024-09-10 10:49:00 +0200

    removed backend endpoint config and env var

--- app/actions.ts
+++ app/actions.ts
@@ -1,6 +1,5 @@
 'use server'
 
-import { ENDPOINT } from 'app/config'
 import { cookies } from 'next/headers'
 
 export type Todo = {
--- app/config.ts
+++
@@ -1,1 +0,0 @@
-export const ENDPOINT = process.env.NEXT_PUBLIC_ENDPOINT as string
